47e834fc4fbd53eda7293882fc4e19b97c880080
JoshdfG <agboolajoshua161@gmail.com>
2024-03-02 07:38:16 +0100

    add animation

--- frontend/src/app/components/home-page-acurast.tsx
+++ frontend/src/app/components/home-page-acurast.tsx
@@ -4,6 +4,8 @@ import { Gluten } from 'next/font/google'
 import Image from 'next/image'
 import { FC } from 'react'
 
+import { Fade } from 'react-awesome-reveal'
+
 import { Button } from '@/components/ui/button'
 
 import heroImg from '../../../public/images/bananas.png'
@@ -23,24 +25,34 @@ export const HomeAcurastSection: FC = () => {
         /> */}
         <div className="mx-auto grid max-w-screen-2xl px-4 pb-8 pt-20 lg:grid-cols-12 lg:gap-8 lg:py-16 lg:pt-28 xl:gap-0">
           <div className="mr-auto place-self-center  lg:col-span-7">
-            <h1 className="mb-4 max-w-3xl text-5xl font-extrabold leading-none tracking-tight text-white md:text-5xl xl:text-6xl">
-              Acurast Decentralized Cloud Platforms
-              <br />
-              {/* <span
+            <Fade direction="left">
+              <h1 className="mb-4 max-w-3xl text-5xl font-extrabold leading-none tracking-tight text-white md:text-5xl xl:text-6xl">
+                Acurast Decentralized Cloud Platforms
+                <br />
+                {/* <span
                 className={`text-5xl font-semibold text-primary md:text-5xl xl:text-6xl ${gluten.className}`}
               >
                 Badger
               </span> */}
-            </h1>
-            <p className="md:text-xllg:mb-8 mb-6 max-w-2xl font-mono font-light text-gray-500 lg:text-2xl">
-              Scale projects up with {"acurast's"} decentralized serverless cloud infrastructure.
-            </p>
+              </h1>
+            </Fade>
+            <Fade direction="left">
+              {' '}
+              <p className="md:text-xllg:mb-8 mb-6 max-w-2xl font-mono font-light text-gray-500 lg:text-2xl">
+                Scale projects up with {"acurast's"} decentralized serverless cloud infrastructure.
+              </p>
+            </Fade>
+
             <div className="mb-4 space-y-4 sm:flex sm:space-x-4 sm:space-y-0">
-              <Button />
+              <Fade direction="left">
+                <Button />
+              </Fade>
             </div>
           </div>
           <div className=" lg:col-span-5 lg:mt-0 lg:flex">
-            <Image src={heroImg} alt="" width={500} height={500} className="h-[95%] w-[95%]" />
+            <Fade direction="right">
+              <Image src={heroImg} alt="" width={500} height={500} className="h-[95%] w-[95%]" />
+            </Fade>
           </div>
         </div>
       </section>
