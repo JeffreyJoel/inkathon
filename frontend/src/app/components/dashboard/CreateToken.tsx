e2728004f6c1708aded0691f646e4057ec79efcd
JeffreyJoel <jowoloko@gmail.com>
2024-03-10 22:20:33 +0100

    add hook for addresses and add loading state

--- frontend/src/app/components/dashboard/CreateToken.tsx
+++ frontend/src/app/components/dashboard/CreateToken.tsx
@@ -244,6 +244,120 @@ export function TokenSelection() {
 }
 
 export function CreateToken() {
+  const { api, activeAccount, activeSigner } = useInkathon()
+  const { contract, address: contractAddress } = useRegisteredContract(ContractIds.Factory)
+  const { typedContract } = useRegisteredTypedContract(ContractIds.Factory, FactoryContract)
+  const [tokenIds, setTokenIds] = useState<any>()
+
+  const [inputValues, setInputValues] = useState({
+    name: '',
+    symbol: '',
+    decimal: 0,
+    supply: 0,
+    description: '',
+  })
+
+  const handleInputChange = (event: any) => {
+    const { name, value } = event.target
+    setInputValues({ ...inputValues, [name]: value })
+  }
+
+  const fetchOwnerTokenIds = async () => {
+    if (!contract || !typedContract || !api) return
+
+    try {
+      const result = await contractQuery(api, '', contract, 'get_owner_token_ids', {}, [
+        activeAccount?.address,
+      ])
+      const { output, isError, decodedOutput } = decodeOutput(
+        result,
+        contract,
+        'get_owner_token_ids',
+      )
+      if (isError) throw new Error(decodedOutput)
+
+      // Alternatively: Fetch it with typed contract instance
+      const typedResult = await typedContract.query.getOwnerTokenIds(activeAccount?.address || '')
+      console.log('Result from typed contract: ', typedResult)
+      setTokenIds(typedResult?.value.ok)
+
+      console.log(tokenIds)
+    } catch (e) {
+      console.error(e)
+      toast.error('Error while fetching token ids. Try again…')
+    } finally {
+      fetchTokenById()
+      // setFetchIsLoading(false)
+    }
+  }
+
+  const fetchTokenById = async () => {
+    if (!contract || !typedContract || !api) return
+
+    try {
+      const result = await contractQuery(api, '', contract, 'get_token_by_id', {}, [
+        tokenIds?.length - 1,
+      ])
+      const { output, isError, decodedOutput } = decodeOutput(result, contract, 'get_token_by_id')
+      if (isError) throw new Error(decodedOutput)
+
+      // Alternatively: Fetch it with typed contract instance
+      const typedResult = await typedContract.query.getTokenById(tokenIds?.length - 1)
+      console.log('Result from fetchTokenById contract: ', typedResult)
+    } catch (e) {
+      console.error(e)
+      toast.error('Error while fetchingTokenById. Try again…')
+    } finally {
+      // setFetchIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchOwnerTokenIds()
+    console.log(tokenIds)
+  }, [typedContract])
+
+  const createToken = async ({
+    supply,
+    name,
+    symbol,
+    decimal,
+    description,
+  }: {
+    supply: number
+    name: string
+    symbol: string
+    decimal: number
+    description: string
+  }) => {
+    if (!activeAccount || !contract || !activeSigner || !api) {
+      toast.error('Wallet not connected. Try again…')
+      return
+    }
+
+    try {
+      const tokenCreate = await contractTxWithToast(
+        api,
+        activeAccount.address,
+        contract,
+        'create_token',
+        {},
+        [supply, name, symbol, description, decimal, PSP22Hash],
+      )
+
+      console.log(tokenCreate.result)
+      console.log(contractAddress)
+
+      // reset()
+      // console.log(supply, name, symbol, decimal)
+    } catch (e) {
+      console.error(e)
+    } finally {
+      fetchOwnerTokenIds()
+    }
+  }
+
+  if (!api) return null
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -266,23 +380,74 @@ export function CreateToken() {
             <Label htmlFor="name" className=" text-right">
               Name
             </Label>
-            <Input id="name" value="" className="mt-2" />
+            <Input
+              id="name"
+              name="name"
+              // value={inputValues.name}
+              onChange={handleInputChange}
+              className="mt-2"
+            />
           </div>
-          <div className=" items-center gap-4">
+          <div className="mb-5 items-center gap-4">
             <Label htmlFor="symbol" className="text-right">
               Symbol
             </Label>
-            <Input id="symbol" value="" className="mt-2" />
+            <Input
+              id="symbol"
+              name="symbol"
+              // value={inputValues.symbol}
+              onChange={handleInputChange}
+              className="mt-2"
+            />
           </div>
           <div className="mb-5 items-center gap-4">
-            <Label htmlFor="description" className="text-right">
+            <Label htmlFor="symbol" className="text-right">
               Description
             </Label>
-            <Input id="description" value="" className="mt-2" />
+            <Input
+              id="description"
+              name="description"
+              onChange={handleInputChange}
+              className="mt-2"
+            />
+          </div>
+
+          <div className="mb-5 items-center gap-4">
+            <Label htmlFor="symbol" className="text-right">
+              Supply
+            </Label>
+            <Input
+              id="supply"
+              name="supply"
+              // value={inputValues.supply}
+              onChange={handleInputChange}
+              className="mt-2"
+            />
+          </div>
+
+          <div className="mb-5 items-center gap-4">
+            <Label htmlFor="symbol" className="text-right">
+              decimal
+            </Label>
+            <Input
+              id="decimal"
+              name="decimal"
+              // value={inputValues.decimal}
+              onChange={handleInputChange}
+              className="mt-2"
+            />
           </div>
         </div>
+
         <DialogFooter>
-          <Button type="submit">Deploy</Button>
+          <Button
+            type="submit"
+            onClick={() => {
+              createToken(inputValues)
+            }}
+          >
+            Deploy
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
